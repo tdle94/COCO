5d295c41fceb118e5c196b0b7d3cda79da1583bf
Tuyen <tuyendle92@gmail.com>
2021-02-25 17:13:15 -0500

    add application container

--- COCO/Container/DefaultContainer.swift
+++ COCO/Container/DefaultContainer.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Swinject
+
+final class DefaultContainer {
+    
+    let container: Container
+    
+    init() {
+        self.container = Container()
+        self.registerViews()
+    }
+    
+}
+
+extension DefaultContainer {
+    func registerViews() {
+        self.container.register(MainView.self) { resolver in
+            MainView()
+        }
+    }
+}
