3c3b068307bda47d16a03114964fc9be605808c7
Tuyen <tuyendle92@gmail.com>
2021-02-25 17:13:32 -0500

    add MainView

--- COCO/Modules/Main/MainView.swift
+++ COCO/Modules/Main/MainView.swift
@@ -5,4 +5,19 @@
 //  Created by Tuyen Le on 2/25/21.
 //
 
-import Foundation
+import UIKit
+
+class MainView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    init() {
+        super.init(nibName: String(describing: MainView.self), bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
