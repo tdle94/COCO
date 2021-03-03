970949ddae31441a08b1438d2e8be9db48f9bfea
Tuyen <tuyendle92@gmail.com>
2021-03-03 13:05:28 -0500

    add repository

--- COCO/Repositories/COCORepositoryImplementation.swift
+++ COCO/Repositories/COCORepositoryImplementation.swift
@@ -5,4 +5,30 @@
 //  Created by Tuyen Le on 2/25/21.
 //
 
-import Foundation
+import Moya
+import RxSwift
+
+class COCORepositoryImplementation: COCORepository {
+    
+    let service: COCOService
+    
+    init(service: COCOService) {
+        self.service = service
+    }
+    
+    func categories(ids: [Int]) -> Single<[Int]> {
+        return service.categories(ids: ids).map([Int].self)
+    }
+    
+    func getImageData(ids: [Int]) -> Single<[ImageData]> {        
+        return service.getImageData(ids: ids, type: .getImages).map([ImageData].self)
+    }
+    
+    func getImageInstance(ids: [Int]) -> Single<[ImageInstance]> {
+        return service.getImageData(ids: ids, type: .getInstances).map([ImageInstance].self)
+    }
+    
+    func getImageCaption(ids: [Int]) -> Single<[ImageCaption]> {
+        return service.getImageData(ids: ids, type: .getCaptions).map([ImageCaption].self)
+    }
+}
