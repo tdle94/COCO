ffae7eb9cd8c023a32fa27009c1b67ff33ffaaef
Tuyen <tuyendle92@gmail.com>
2021-03-03 13:08:20 -0500

    register repositories and services

--- COCO/Container/DefaultContainer.swift
+++ COCO/Container/DefaultContainer.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Swinject
+import Moya
 
 final class DefaultContainer {
     
@@ -15,6 +16,8 @@ final class DefaultContainer {
     init() {
         self.container = Container()
         self.registerViews()
+        self.registerRepositories()
+        self.registerService()
     }
     
 }
@@ -22,7 +25,33 @@ final class DefaultContainer {
 extension DefaultContainer {
     func registerViews() {
         self.container.register(MainView.self) { resolver in
-            MainView()
+            MainView(repository: resolver.resolve(COCORepository.self)!,
+                     localStorage: resolver.resolve(LocalStorage.self)!)
         }
+    }
+    
+    func registerRepositories() {
+        self.container.register(COCORepository.self) { resolver in
+            COCORepositoryImplementation(service: resolver.resolve(COCOService.self)!)
+        }
+        
+        self.container.register(LocalStorage.self) { resolver in
+            LocalSotrageImplementation()
+        }
+    }
+    
+    func registerService() {
+        self.container.register(COCOService.self) { resolver in
+            let provider = MoyaProvider<COCORouter>(plugins: self.getDefaultPlugins())
+            return COCOServiceImplementation(provider: provider)
+        }
+    }
+    
+    func getDefaultPlugins() -> [PluginType] {
+        #if DEBUG
+            return [NetworkLoggerPlugin(verbose: true)]
+        #else
+            return []
+        #endif
     }
 }
